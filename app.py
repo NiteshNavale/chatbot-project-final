e8c04c34d7fcdc4f9620a203bf5cfdd9a689dafe
Nitesh navale <38331805+NiteshNavale@users.noreply.github.com>
2025-08-04 12:16:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 
 # CORRECT import line for the refined chain type
-from langchain.chains.Youtubeing import load_qa_chain
+
 from langchain_core.prompts import PromptTemplate
 
 def extract_text_from_file(file):
